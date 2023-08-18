4699c543107e8b01c7a1180ed80af7155b14b8e1
David Alonso <davidoort@hotmail.com>
2023-08-18 18:07:26 +0200

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,19 +5,35 @@ echo "$INPUT_SECRETS" > /secrets.json
 gcloud auth activate-service-account --key-file=/secrets.json
 rm /secrets.json
 
-# Create temporary directory for syncing
-mkdir /tmp/sync-dir
+# Create temp dir
+rm -rf /tmp/sync-dir/
+mkdir -p /tmp/sync-dir
 
 # Include files based on the pattern
-echo "Including files: $INPUT_INCLUDE ..."
-find /github/workspace/ -regex "$INPUT_INCLUDE" -exec cp --parents \{\} /tmp/sync-dir \;
+include_files="$INPUT_INCLUDE"
+
+# Split patterns
+patterns=(${include_files//|/ })
+
+# Copy files and create directories
+for pattern in "${patterns[@]}"; do
+  # Get directory from pattern
+  dir=$(dirname "$pattern")
+
+  # Create dir in tmp folder
+  mkdir -p "/tmp/sync-dir/$dir"
+
+  # Copy files
+  find /github/workspace/ -regex "$pattern" -exec cp --parents \{\} "/tmp/sync-dir/$dir/" \;
+
+done
 
 # Sync files to bucket
 echo "Syncing bucket $INPUT_BUCKET ..."
-gsutil -m rsync -r -c -d /tmp/sync-dir/ gs://$INPUT_BUCKET/$INPUT_SYNC_DIR
+gsutil rsync -r -c -d /tmp/sync-dir/ gs://$INPUT_BUCKET/$INPUT_SYNC_DIR
 if [ $? -ne 0 ]; then
-    echo "Syncing failed"
-    exit 1
+  echo "Syncing failed"
+  exit 1
 fi
 
 # Clean up temporary directory
